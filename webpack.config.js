cdd17e5a283dabce485faae982dcec3a3b349e26
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-02-27 13:23:54 +0300

    update webpackconfi

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports={
             {
                 test:/\.css$/i,
                 use:['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg)$/i,
+                type: "asset/resource"
             }
         ]
     }
