ed5a19b94cc1decfdaff6af56c6d70bdaf220af0
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-02-28 10:05:42 +0300

    add some function on eventlistener

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
+import aboutPage from "./about-page";
 import homePage from "./home-page";
+import menuPage from "./menu-page";
 import './style.css'
 
+const btnHome = document.getElementById('home')
+const btnMenu=document.getElementById('menu')
+const btnAbout=document.getElementById('about')
+
+btnHome.addEventListener('click',()=>{
+    cleanContent()
+    removeClassList()
+    homePage()
+    btnHome.setAttribute('class','selected')
+}) 
+btnMenu.addEventListener('click',()=>{
+    cleanContent()
+    removeClassList()
+    menuPage()
+    btnMenu.setAttribute('class','selected')
+})
+btnAbout.addEventListener('click',()=>{
+    cleanContent()
+    removeClassList()
+    aboutPage()
+    btnAbout.setAttribute('class','selected')
+})
+
 homePage()
+
+function cleanContent(){
+    content.innerText=''
+}
+
+function removeClassList(){
+    btnHome.classList=''
+    btnMenu.classList=''
+    btnAbout.classList=''
+}
